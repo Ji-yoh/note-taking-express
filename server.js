86384400b98c869e471c3aafa49d7e3912ee1280
Jiyoh Macbook <geovannih514@gmail.com>
2023-10-03 23:00:29 -0700

    refactored server.js to add read db.json and add notes without refreshing, delete function works but is still buggy

--- server.js
+++ server.js
@@ -24,68 +24,58 @@ app.get('/', (req, res) => {
 // this should return notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
-    //res.status(200).json(db);
-    console.log(db);
+    // res.status(200).json(db);
+    // console.log(db);
 });
 
 // added get route to read db.json for user notes
 app.get('/api/notes', (req, res) => {
-    res.json(db);
-})
+    const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    res.json(notes);
+});
 
+// refactored to use fs.writeFileSync instead of fs.writeFile and remove extra code
 // post route to add new notes to db.json
 app.post('/api/notes', (req, res) => {
     const { title, text } = req.body;
     // note body, id should be a random number
-    const newNote = {
-        title,
-        text,
-        id: Math.floor((1 + Math.random()) * 0x1000)
-    }
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: Math.floor((1 + Math.random()) * 0x1000)
+        }
     // get existing notes from db.json and add new notes to it
     // corrected path to db.json
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            const notes = JSON.parse(data);
-            notes.push(newNote); // add new notes
-            fs.writeFile('./db/db.json', JSON.stringify(notes, null, 4), (err) => 
-            err ? console.error(err) : console.log('New note added!'))
-            // have notes show on notes.html
-
-        }
-    })
-    const response = {
-        status: 'success',
-        body: newNote,
-    };
-
-    if (response.status === 'success') {
-        console.log(response);
-        res.status(201).json(response);
-    } else {
-        res.status(500).json('Error in posting note');
-    }
+    db.push(newNote); // add new notes
+    fs.writeFileSync('./db/db.json', JSON.stringify(db, null, 4), (err) => 
+    err ? console.error(err) : console.log('New note added!'))
+    // have notes show on notes.html
+    res.json(db);
+    } 
 });
 
 // try to delete notes? ..notes are being deleted but need to restart server to see changes
 app.delete('/api/notes/:id', (req, res) => {
     const id = req.params.id; // get note id from db.json
+    const deleteNote = db.find((note) => note.id === id); // find note to delete
+    db.splice(deleteNote, 1); // delete note
+    fs.writeFileSync('./db/db.json', JSON.stringify(db, null, 4))
+    res.json(db); // return db.json
     // console.log(id);
-    if (id) {
+    /*if (id) {
         fs.readFile('./db/db.json', 'utf8', (err, data) => {
             if (err) {
                 console.error(err);
             } else {
                 const savedNotes = JSON.parse(data)
-                const newNotes = savedNotes.filter((note) => note.id != id)
-                fs.writeFile('./db/db.json', JSON.stringify(newNotes, null, 4), (err) => {
+                const deletedNotes = savedNotes.filter((note) => note.id != id)
+                fs.writeFile('./db/db.json', JSON.stringify(deletedNotes, null, 4), (err) => {
                     err ? console.error(err) : console.log('Note deleted!')
                 })
             }
         })
-    }
+    } */
 })
 
 app.listen(3000, () => console.log(`App listening at http://localhost:${PORT}`))
